e554465c8253cbaae4ccda27b77ec9982b199837
Xuan <Xuan@cang.com>
2022-09-23 14:29:28 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -9,5 +9,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git！！！");
         System.out.println("hello git2!!!");
+        System.out.println("hello git3！！！");
     }
 }
