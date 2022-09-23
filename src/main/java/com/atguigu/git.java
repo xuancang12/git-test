9d63cc3415b8fcd8b9976297549f02819d856fc2
Xuan <Xuan@cang.com>
2022-09-23 14:36:59 +0800

    master commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -10,5 +10,6 @@ public class git {
         System.out.println("hello git！！！");
         System.out.println("hello git2!!!");
         System.out.println("hello git3！！！");
+        System.out.println("master test!");
     }
 }
