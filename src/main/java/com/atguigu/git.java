fe7aecef6f4713e788bbae1f1e616dbbf9026e36
Xuan <Xuan@cang.com>
2022-09-23 10:48:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * 黄洋林: Huang
+ * 2022/9/23 10:31
+ */
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git！！！");
+    }
+}
